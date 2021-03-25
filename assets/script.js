7bd92eff5635500e7f7ade2000d674249f23d2be
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-03-24 23:33:52 -0400

    This is my last commit message as time expires. formatted js and HTML with Prettier. So much to code, so little time!

--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,69 @@
-//  DOM VARIABLES 
+//  DOM VARIABLES
 var questionContainer = document.getElementById("start-page");
 var correctAnswerButton = document.getElementById("correct-answer");
-var incorrectAnswerButtonAlpha = document.getElementById("incorrect-answer-alpha");
-var incorrectAnswerButtonBeta = document.getElementById("incorrect-answer-beta");
-var incorrectAnswerButtonCharlie = document.getElementById("incorrect-answer-charlie");
-// JAVASCRIPT VARIABLES 
+var incorrectAnswerButtonAlpha = document.getElementById(
+  "incorrect-answer-alpha"
+);
+var incorrectAnswerButtonBeta = document.getElementById(
+  "incorrect-answer-beta"
+);
+var incorrectAnswerButtonCharlie = document.getElementById(
+  "incorrect-answer-charlie"
+);
+// JAVASCRIPT VARIABLES
 var arrayOfQuestions = [
-    "Commonly used data types DO NOT include:",
-    "The condition in an if/else statement is enclosed within ____.",
-    "Arrays in JavaScript can be used to store ____.",
-    "String values must be enclosed within ____ when being assigned to variables.",
-    "A very useful tool used during development and debugging for printing content to the debugger is:",
+  "Commonly used data types DO NOT include:",
+  "The condition in an if/else statement is enclosed within ____.",
+  "Arrays in JavaScript can be used to store ____.",
+  "String values must be enclosed within ____ when being assigned to variables.",
+  "A very useful tool used during development and debugging for printing content to the debugger is:",
 ];
 var currentIndex = 0;
 // FUNCTION DEFINITIONS
 
-function displayQuestion(){
-    // clears out previous question
-    questionContainer.innerHTML = "";
-    // element
-    var questionEl = document.createElement("h1");
-    // add content
-    questionEl.textContent = arrayOfQuestions[currentIndex];
-    // append to existing element
-    questionContainer.append(questionEl);
+function displayQuestion() {
+  // clears out previous question
+  questionContainer.innerHTML = "";
+  // element
+  var questionEl = document.createElement("h1");
+  // add content
+  questionEl.textContent = arrayOfQuestions[currentIndex];
+  // append to existing element
+  questionContainer.append(questionEl);
 }
 
 // EVENT LISTENERS
-correctAnswerButton.addEventListener("click", function(){
-    console.log("You choose wisely");
-    // ran a check to cycle through index and test. marked out as note when test successful - b/c I want quiz to have a beginning and conclusion.
-    // if(currentIndex < arrayOfQuestions.length - 1){
-        currentIndex++;
-    // } else {
-    //     currentIndex = 0;
-    // }
-    displayQuestion();
-})
-incorrectAnswerButtonAlpha.addEventListener("click", function(){
-    console.log("You choose poorly");
-         
-         currentIndex++;
+correctAnswerButton.addEventListener("click", function () {
+  console.log("You choose wisely");
+  // ran a check to cycle through index and test. marked out as note when test successful - b/c I want quiz to have a beginning and conclusion.
+  // if(currentIndex < arrayOfQuestions.length - 1){
+  currentIndex++;
+  // } else {
+  //     currentIndex = 0;
+  // }
+  displayQuestion();
+});
+incorrectAnswerButtonAlpha.addEventListener("click", function () {
+  console.log("You choose poorly");
 
-    displayQuestion();
-})
-incorrectAnswerButtonBeta.addEventListener("click", function(){
-    console.log("You choose poorly");
-         
-         currentIndex++;
+  currentIndex++;
 
-    displayQuestion();
-})
-incorrectAnswerButtonCharlie.addEventListener("click", function(){
-    console.log("You choose poorly");
-         
-         currentIndex++;
+  displayQuestion();
+});
+incorrectAnswerButtonBeta.addEventListener("click", function () {
+  console.log("You choose poorly");
 
-    displayQuestion();
-})
+  currentIndex++;
+
+  displayQuestion();
+});
+incorrectAnswerButtonCharlie.addEventListener("click", function () {
+  console.log("You choose poorly");
+
+  currentIndex++;
+
+  displayQuestion();
+});
 // FUNCTION CALLS
 displayQuestion();
 
