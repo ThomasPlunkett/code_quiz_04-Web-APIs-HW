0710aa3ec817637c75685f5afc3283cee566d376
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-03-24 21:23:07 -0400

    upon test, realized several errors. I removed text from h1 and removed the h3 with text. I will need to add the start quiz text somewhere, i think the array of questions. I may need to then rename array pf questions and change the function language to match. after removing this, my array did display the questions correctly, although at this stage i must manually cycle through the index. onward

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ var questionContainer = document.getElementById("start-page");
 var arrayOfQuestions = [
     "Commonly used data types DO NOT include:",
     "The condition in an if/else statement is enclosed within ____.",
-    "Arrays in JavaSCript can be used to store ____.",
+    "Arrays in JavaScript can be used to store ____.",
     "String values must be enclosed within ____ when being assigned to variables.",
     "A very useful tool used during development and debugging for printing content to the debugger is:",
 ];
@@ -15,7 +15,7 @@ var arrayOfQuestions = [
 function displayQuestion(){
     var questionEl = document.createElement("h1");
 
-    questionEl.textContent = arrayOfQuestions[0];
+    questionEl.textContent = arrayOfQuestions[2];
 
     questionContainer.append(questionEl);
 }
