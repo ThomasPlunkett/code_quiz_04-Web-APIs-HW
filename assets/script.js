1760aaa7f84743c60b19647900b15a84d6046d00
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-03-24 20:54:06 -0400

    modified script.js to add and array of questions shown in the gif.

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,13 @@
 var h1El = document.getElementById("start-page");
 
 // JAVASCRIPT VARIABLES 
+var arrayOfQuestions = [
+    "Commonly used data types DO NOT include:",
+    "The condition in an if/else statement is enclosed within ____.",
+    "Arrays in JavaSCript can be used to store ____.",
+    "String values must be enclosed within ____ when being assigned to variables.",
+    "A very useful tool used during development and debugging for printing content to the debugger is:",
+];
 
 // FUNCTION DEFINITIONS
 
